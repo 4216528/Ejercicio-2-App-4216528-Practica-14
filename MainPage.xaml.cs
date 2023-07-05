e3f0b96ccdeb2b8128ab11bae3ce010068021b7d
NERY DAVID MENDEZ <4216528@estudiantes.mined.edu.sv>
2023-07-05 12:13:40 -0600

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace Ejercicio2;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+
+    {
+
+        InitializeComponent();
+
+    }
+
+    ///<Summary>
+    ///<Createddate>03-07-2023</createddate>
+    ///<company>SandBox</company>
+    ///<lastmodificationdate>03-07-2023</lastmodificationdate>
+    ///<lastmodificationdescription>Ninguna</lastmodificationdescription>
+    ///<lastmodifierautor>Nery</lastmodifierautor>
+    ///</Summary>
+
+    public void OnCounterClicked(object sender, EventArgs e)
+
+    {
+
+        // Mostramos el resultado con la propiedad DisplayAlert y un mensaje personalizado //
+
+        double A = double.Parse(AEntry.Text);
+
+        double B = double.Parse(BEntry.Text);
+
+        double result = Math.Pow(A + B, 2) / 2;
+
+        Resultado.Text = result.ToString();
+
+    }
+}
